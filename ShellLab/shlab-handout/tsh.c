14b45220bd44a80ca66e8190010c478128a78f88
hekewen <821200725@qq.com>
2017-10-04 18:39:43 +0800

    reconfig the project

--- ShellLab/shlab-handout/tsh.c
+++ ShellLab/shlab-handout/tsh.c
@@ -13,6 +13,8 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <errno.h>
+#include "funwitherr.h"
+#include "jobs.h"
 
 
 
@@ -20,46 +22,14 @@
 /* Misc manifest constants */
 #define MAXLINE    1024   /* max line size */
 #define MAXARGS     128   /* max args on a command line */
-#define MAXJOBS      16   /* max jobs at any point in time */
-#define MAXJID    1<<16   /* max job ID */
 
-/* Job states */
-#define UNDEF 0 /* undefined */
-#define FG 1    /* running in foreground */
-#define BG 2    /* running in background */
-#define ST 3    /* stopped */
-
-
-/* Sleep for a random period between [0,MAX_SLEEP] us */
-#define MAX_SLEEP 100000
-
-/* Macro that maps val into the range [0,RAND_MAX] */
-#define CONVERT(val) (((double)val)/(double)RAND_MAX)
-
-/* 
- * Jobs states: FG (foreground), BG (background), ST (stopped)
- * Job state transitions and enabling actions:
- *     FG -> ST  : ctrl-z
- *     ST -> FG  : fg command
- *     ST -> BG  : bg command
- *     BG -> FG  : fg command
- * At most 1 job can be in the FG state.
- */
 
 /* Global variables */
 extern char **environ;      /* defined in libc */
 char prompt[] = "tsh> ";    /* command line prompt (DO NOT CHANGE) */
-int verbose = 0;            /* if true, print additional output */
-int nextjid = 1;            /* next job ID to allocate */
+//extern int verbose;            /* if true, print additional output */
 char sbuf[MAXLINE];         /* for composing sprintf messages */
 
-struct job_t {              /* The job struct */
-    pid_t pid;              /* job PID */
-    int jid;                /* job ID [1, 2, ...] */
-    int state;              /* UNDEF, BG, FG, or ST */
-    char cmdline[MAXLINE];  /* command line */
-};
-struct job_t jobs[MAXJOBS]; /* The job list */
 
 
 
@@ -86,20 +56,8 @@ void sigint_handler(int sig);
 int parseline(const char *cmdline, char **argv); 
 void sigquit_handler(int sig);
 
-void clearjob(struct job_t *job);
-void initjobs(struct job_t *jobs);
-int maxjid(struct job_t *jobs); 
-int addjob(struct job_t *jobs, pid_t pid, int state, char *cmdline);
-int deletejob(struct job_t *jobs, pid_t pid); 
-pid_t fgpid(struct job_t *jobs);
-struct job_t *getjobpid(struct job_t *jobs, pid_t pid);
-struct job_t *getjobjid(struct job_t *jobs, int jid); 
-int pid2jid(pid_t pid); 
-void listjobs(struct job_t *jobs);
-
 void usage(void);
-void unix_error(char *msg);
-void app_error(char *msg);
+
 typedef void handler_t(int);
 handler_t *Signal(int signum, handler_t *handler);
 
@@ -171,82 +129,9 @@ int main(int argc, char **argv)
 }
   
 
-//对fork()的警告进行封装
-//对一个fork的包装函数，它随机地决定父进程和子进程执行的顺序。父进程和
-//子进程扔一枚硬币来决定谁会休眠，因而给另一个进程被调度的机会
-//及尽可能的产生竞争条件的情况，来进行问题检测
-pid_t Fork(void)
-{
-    static struct timeval time;
-    unsigned boo1,secs;
-    pid_t pid;
-
-    /* Generate a different seed each time the function is called */
-    //获取当前时间，利用当前时间来产生随机种子
-    gettimeofday(&time,NULL);
-    srand(time.tv_usec);
-
-    /* Determine whether to sleep in parent of child and for how long */
-    //决定那个进程睡眠
-    boo1=(unsigned)(CONVERT(rand())+0.5);
-    //决定该进程睡眠多久
-    secs=(unsigned)(CONVERT(rand())*MAX_SLEEP);
-
-    /* Call the real fork function */
-    if((pid=fork())<0)
-        return pid;
-    
-    if(pid==0) //Child
-    {
-        if(boo1)
-        {
-            usleep(secs);
-        }
-    }
-    else    //Parent
-    {
-        if(!boo1)
-        {
-            usleep(secs);
-        }
-    }
-
-    /* Return the PID like a normal fork call */
-    return pid;
-}
 
 
-//针对sigemptyset进行警告封装
-void Sigemptyset(sigset_t *mask)
-{
-    if(sigemptyset(mask)!=0)
-        unix_error("sigemptyset error");
-}
 
-//针对sigaddset()进行警告封装
-void Sigaddset(sigset_t *mask,int signum)
-{
-    if(sigaddset(mask,signum)!=0)
-        unix_error("sigaddset error!");
-}
-
-//针对sigprocmask()进行警告封装
-void Sigprocmask(int how, const sigset_t * set, sigset_t * oldset)
-{
-    if(sigprocmask(how,set,oldset)!=0)
-        unix_error("sigprocmask error!");
-}
-
-
-//execve with error
-void Execve(char *string,char **argv,char ** environ)
-{
-    if(execve(argv[0],argv,environ)<0) 
-    {   
-        printf("%s: Command not found.\n",argv[0]);
-        exit(0);
-    }   
-}
 
 /* 
  * eval - Evaluate the command line that the user has just typed in
@@ -286,8 +171,6 @@ void eval(char *cmdline)
         //必须放在builtin_cmd里面，因为如果放在外面，在执行内部命令的时候，就不会释放这些锁了
         Sigemptyset(&mask);
         Sigaddset(&mask,SIGCHLD);
-//        Sigaddset(&mask,SIGINT);
-//        Sigaddset(&mask,SIGTSTP);
         Sigprocmask(SIG_BLOCK,&mask,NULL);
 
         if((pid=Fork())==0)         //创建子进程,Running the child process.
@@ -633,158 +516,7 @@ void sigtstp_handler(int sig)
  * End signal handlers
  *********************/
 
-/***********************************************
- * Helper routines that manipulate the job list
- **********************************************/
 
-/* clearjob - Clear the entries in a job struct */
-void clearjob(struct job_t *job) {
-    job->pid = 0;
-    job->jid = 0;
-    job->state = UNDEF;
-    job->cmdline[0] = '\0';
-}
-
-/* initjobs - Initialize the job list */
-void initjobs(struct job_t *jobs) {
-    int i;
-
-    for (i = 0; i < MAXJOBS; i++)
-	clearjob(&jobs[i]);
-}
-
-/* maxjid - Returns largest allocated job ID */
-int maxjid(struct job_t *jobs) 
-{
-    int i, max=0;
-
-    for (i = 0; i < MAXJOBS; i++)
-	if (jobs[i].jid > max)
-	    max = jobs[i].jid;
-    return max;
-}
-
-/* addjob - Add a job to the job list */
-int addjob(struct job_t *jobs, pid_t pid, int state, char *cmdline) 
-{
-    int i;
-    
-    if (pid < 1)
-	return 0;
-
-    for (i = 0; i < MAXJOBS; i++) {
-	if (jobs[i].pid == 0) {
-	    jobs[i].pid = pid;
-	    jobs[i].state = state;
-	    jobs[i].jid = nextjid++;
-	    if (nextjid > MAXJOBS)
-		nextjid = 1;
-	    strcpy(jobs[i].cmdline, cmdline);
-  	    if(verbose){
-	        printf("Added job [%d] %d %s\n", jobs[i].jid, jobs[i].pid, jobs[i].cmdline);
-            }
-            return 1;
-	}
-    }
-    printf("Tried to create too many jobs\n");
-    return 0;
-}
-
-/* deletejob - Delete a job whose PID=pid from the job list */
-int deletejob(struct job_t *jobs, pid_t pid) 
-{
-    int i;
-
-    if (pid < 1)
-	return 0;
-
-    for (i = 0; i < MAXJOBS; i++) {
-	if (jobs[i].pid == pid) {
-	    clearjob(&jobs[i]);
-	    nextjid = maxjid(jobs)+1;
-	    return 1;
-	}
-    }
-    return 0;
-}
-
-/* fgpid - Return PID of current foreground job, 0 if no such job */
-pid_t fgpid(struct job_t *jobs) {
-    int i;
-
-    for (i = 0; i < MAXJOBS; i++)
-	if (jobs[i].state == FG)
-	    return jobs[i].pid;
-    return 0;
-}
-
-/* getjobpid  - Find a job (by PID) on the job list */
-struct job_t *getjobpid(struct job_t *jobs, pid_t pid) {
-    int i;
-
-    if (pid < 1)
-	return NULL;
-    for (i = 0; i < MAXJOBS; i++)
-	if (jobs[i].pid == pid)
-	    return &jobs[i];
-    return NULL;
-}
-
-/* getjobjid  - Find a job (by JID) on the job list */
-struct job_t *getjobjid(struct job_t *jobs, int jid) 
-{
-    int i;
-    if (jid < 1)
-	return NULL;
-    for (i = 0; i < MAXJOBS; i++)
-	if (jobs[i].jid == jid)
-	    return &jobs[i];
-    return NULL;
-}
-
-/* pid2jid - Map process ID to job ID */
-int pid2jid(pid_t pid) 
-{
-    int i;
-
-    if (pid < 1)
-	return 0;
-    for (i = 0; i < MAXJOBS; i++)
-	if (jobs[i].pid == pid) {
-            return jobs[i].jid;
-        }
-    return 0;
-}
-
-/* listjobs - Print the job list */
-void listjobs(struct job_t *jobs) 
-{
-    int i;
-    
-    for (i = 0; i < MAXJOBS; i++) {
-	if (jobs[i].pid != 0) {
-	    printf("[%d] (%d) ", jobs[i].jid, jobs[i].pid);
-	    switch (jobs[i].state) {
-		case BG: 
-		    printf("Running ");
-		    break;
-		case FG: 
-		    printf("Foreground ");
-		    break;
-		case ST: 
-		    printf("Stopped ");
-		    break;
-	    default:
-		    printf("listjobs: Internal error: job[%d].state=%d ", 
-			   i, jobs[i].state);
-	    }
-	    printf("%s", jobs[i].cmdline);
-	}
-    }
-}
-/******************************
- * end job list helper routines
- ******************************/
 
 
 /***********************
@@ -803,23 +535,7 @@ void usage(void)
     exit(1);
 }
 
-/*
- * unix_error - unix-style error routine
- */
-void unix_error(char *msg)
-{
-    fprintf(stdout, "%s: %s\n", msg, strerror(errno));
-    exit(1);
-}
 
-/*
- * app_error - application-style error routine
- */
-void app_error(char *msg)
-{
-    fprintf(stdout, "%s\n", msg);
-    exit(1);
-}
 
 /*
  * Signal - wrapper for the sigaction function
